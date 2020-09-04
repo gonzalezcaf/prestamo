6f3e46f303f9356d0baea893643b9c35de3dfcad
fgonzalezc <>
2020-09-04 09:57:11 -0500

    {/src/main/java/com/learning/prestamo/solicitud.java}

--- src/main/java/com/learning/prestamo/solicitud.java
+++ src/main/java/com/learning/prestamo/solicitud.java
@@ -6,12 +6,63 @@ package com.learning.prestamo;
 
 public class solicitud implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public solicitud() {
-    }
+	@org.kie.api.definition.type.Description(value = "Numero de solicitud de prestamo")
+	@org.kie.api.definition.type.Label(value = "Id Solicitud")
+	private java.lang.Integer idSolicitud;
+	@org.kie.api.definition.type.Description(value = "Monto de dinero solicitado")
+	@org.kie.api.definition.type.Label(value = "Monto Solicitado")
+	private java.lang.Integer monto;
+	@org.kie.api.definition.type.Description(value = "Monto Final Aprobado")
+	@org.kie.api.definition.type.Label(value = "Monto Aprobado")
+	private java.lang.Integer montoaprobado;
+	@org.kie.api.definition.type.Description(value = "Cliente que solicita el prestamo")
+	@org.kie.api.definition.type.Label(value = "Cliente")
+	private com.learning.prestamo.cliente cliente;
 
+	public solicitud() {
+	}
 
+	public java.lang.Integer getIdSolicitud() {
+		return this.idSolicitud;
+	}
 
+	public void setIdSolicitud(java.lang.Integer idSolicitud) {
+		this.idSolicitud = idSolicitud;
+	}
+
+	public java.lang.Integer getMonto() {
+		return this.monto;
+	}
+
+	public void setMonto(java.lang.Integer monto) {
+		this.monto = monto;
+	}
+
+	public java.lang.Integer getMontoaprobado() {
+		return this.montoaprobado;
+	}
+
+	public void setMontoaprobado(java.lang.Integer montoaprobado) {
+		this.montoaprobado = montoaprobado;
+	}
+
+	public com.learning.prestamo.cliente getCliente() {
+		return this.cliente;
+	}
+
+	public void setCliente(com.learning.prestamo.cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public solicitud(java.lang.Integer idSolicitud, java.lang.Integer monto,
+			java.lang.Integer montoaprobado,
+			com.learning.prestamo.cliente cliente) {
+		this.idSolicitud = idSolicitud;
+		this.monto = monto;
+		this.montoaprobado = montoaprobado;
+		this.cliente = cliente;
+	}
 
 }
